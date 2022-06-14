36396e53b0883c2014575fa0e6e8c0594f00f16a
Abreham Atlaw <abrishatlaw@gmail.com>
2022-06-14 22:55:54 +0300

    Implemented Agent Following Camera

--- core/main.py
+++ core/main.py
@@ -1,14 +1,17 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
-from panda3d.core import Point3
+from panda3d.core import Point3, NodePath
+from direct.interval.IntervalGlobal import Sequence, Func, Wait
 
 import gltf
 
 import os
 
+from core.utils.keymapping import KeyMapping
 from core import Config
 
-from .agent import MazeRunnerAgent, AgentManager
+from .agent import RhinoAgent, AgentManager
+
 
 class MazeRunnerGame(ShowBase):
 
@@ -16,16 +19,20 @@ class MazeRunnerGame(ShowBase):
 		super().__init__()
 		
 		gltf.patch_loader(self.loader)
+		
+		self.key_mapping = KeyMapping.load(Config.KEY_MAPPING_PATH)
 
 		self.level = 0
 		self.start_level(self.level)
-
+		
 	def start_level(self, level):
 		scene = self.setup_scene(level)
 		scene.reparentTo(self.render)
 
 		agent = self.setup_agent("rhino")
-		agent.reparentTo(self.render)
+		agent.reparentTo(scene)
+
+		self.setup_camera(agent)
 	
 	def setup_scene(self, level):
 		scene = self.loader.loadModel(os.path.join(Config.RES_PATH,
@@ -36,19 +43,27 @@ class MazeRunnerGame(ShowBase):
 
 	def setup_agent(self, agent):
 
-		agent = MazeRunnerAgent(
-					os.path.join(Config.RES_PATH,"models/agents/rhino/scene.gltf"),
-					step_size = 30,
-					step_duration = 10
-				)
-		agent.setScale(0.0003, 0.0003, 0.0003)
-		agent.setPos(1, 5, -1)
-		agent.setHpr(90, 270, 0)
-		
-		#agent.posInterval(5, Point3(1, 100, -1), startPos=(1, 0, -1))
-		#agent.loop("walk")
-		agent_manager = AgentManager(agent)
-
+		agent = RhinoAgent()
+		agent_manager = AgentManager(agent, self.key_mapping)
 		return agent
 
+	def setup_camera(self, agent):
+		self.disableMouse()
+		
+		agent_scale = agent.getScale()[0]
+		
+		camera_parent = NodePath("Camera Parent")
+		camera_parent.reparentTo(agent)
+		camera_parent.setHpr(-agent.getH(), 0, -agent.getP())
+
+		self.camera.reparentTo(camera_parent)
+		self.camera.setScale(1/agent_scale)
+		self.camera.setPos(0, -20/agent_scale, 10/agent_scale)
+		self.camera.lookAt(*agent.getPos())
+
+
+	def cam_task(self, task):
+		print("Cam Pos: %s" % (self.camera.getPos(self.render)))
+		self.disableMouse()
+		self.camera.setPos(0, 0, 25)
 
